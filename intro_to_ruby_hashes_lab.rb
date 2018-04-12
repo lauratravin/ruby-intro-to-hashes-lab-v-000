3e35e67526dcca935faabbeea77b940c36046e11
Laura Messa Travin <laura.travin@gmail.com>
2018-04-12 03:38:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
 	 monopoly[:railroads][:names][:pennsylvania_railroad] = {}
    monopoly[:railroads][:names][:b_and_o_railroad] = {}
 	 monopoly[:railroads][:names][:shortline] = {}
-	monopoly
+	 monopoly
 
 end
 
